20ad94ce8f05cfbe797404be44d8de3371ecbb11
rozatoo <69800109+rozatoo@users.noreply.github.com>
2020-09-07 14:40:54 +0200

    /Wiki command
    Tried in implement the suggestion of adding a /wiki command 
    No fucking clue if it works lmao

--- src/main/java/com/akoot/plugins/ultravanilla/commands/WikiCommand.java
+++ src/main/java/com/akoot/plugins/ultravanilla/commands/WikiCommand.java
@@ -9,17 +9,23 @@ import org.bukkit.command.TabExecutor;
 
 import java.util.List;
 
-public class TemplateCommand extends UltraCommand implements CommandExecutor, TabExecutor {
+public class KitCommand extends UltraCommand implements CommandExecutor, TabExecutor {
 
-    public static final ChatColor COLOR = ChatColor.WHITE;
+    public static final ChatColor COLOR = ChatColor.Green;
 
-    public TemplateCommand(UltraVanilla instance) {
+    public KitCommand(UltraVanilla instance) {
         super(instance);
         this.color = COLOR;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+      if (args.length == 0) {
+                  if (sender instanceof Player) {
+                      Player player = (Player) sender;
+                      sendMessage(sender, "Check out our Playermanaged Wiki!");
+                      sendMessage(sender, "https://uv.miraheze.org/wiki/Main_Page");
+
         return false;
     }
 
